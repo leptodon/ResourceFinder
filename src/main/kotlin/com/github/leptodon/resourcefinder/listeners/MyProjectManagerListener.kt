3abd9e15c5e377c11f74f69310db1c73d8944a9e
GitHub Action <action@github.com>
2022-11-22 21:32:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/leptodon/resourcefinder/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/leptodon/resourcefinder/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.leptodon.resourcefinder.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.leptodon.resourcefinder.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/leptodon/resourcefinder/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/leptodon/resourcefinder/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.leptodon.resourcefinder.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.leptodon.resourcefinder.MyBundle
 
 class MyApplicationService {
 
